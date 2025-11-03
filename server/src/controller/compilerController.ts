5743e97681db8c8b6800b9e671382f4084d83499
rahul sahu <rahulsahu770121@gmail.com>
2025-11-03 11:16:38 +0530

    impliment save code logic in backend

--- server/src/controller/compilerController.ts
+++ server/src/controller/compilerController.ts
@@ -1,7 +1,23 @@
 import { Request, Response } from "express";
+import { codeModel } from "../models/codeSchema";
 
-const saveCode = (req: Request, res: Response) => {
-    res.send("save request come");
+const saveCode = async (req: Request, res: Response) => {
+    
+  const body = req.body || {};
+  const { fullCode } = body as { fullCode?: string };
+
+  if (!fullCode) {
+    return res.status(400).json({ success: false, message: "fullCode is required in request body" });
+  }
+
+  try {
+    const newCode = await codeModel.create({
+      fullCode: fullCode,
+    });
+    return res.status(200).json({ success: true,url:newCode._id,status:"saved"});
+  } catch (error) {
+    return res.status(500).json({ success: false, message: "failed while save", status:"unsaved" });
+  }
 };
 
 export { saveCode };
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,17 +1,28 @@
 import express, { Request, Response } from "express";
+import cors from "cors";
 import compileRouter from "./routes/compilerRoutes";
 import { config } from "dotenv";
 import connectDB from "./config/dbConnect";
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(
+  cors({
+    credentials: true,
+    origin: ["http://localhost:5173", process.env.CLIENT_URL!],
+  })
+);
 config();
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello from TypeScript Server 🚀");
 });
-
 app.use("/compile", compileRouter);
 
-app.listen(process.env.PORT, async () => {
+const PORT = process.env.PORT || 4000;
+
+app.listen(PORT, async () => {
   await connectDB();
-  console.log(`Server running on http://localhost:${process.env.PORT}`);
+  console.log(`Server running on http://localhost:${PORT}`);
 });

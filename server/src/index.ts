9386df77e380824aa4526a43a8c7cbac3405fc73
rahul sahu <rahulsahu770121@gmail.com>
2025-11-20 09:48:16 +0530

    fix bug

--- server/src/index.ts
+++ server/src/index.ts
@@ -25,7 +25,8 @@ const corsOptions = {
 };
 
 app.use(cors(corsOptions));
-app.options("*", cors(corsOptions));
+
+app.options("/*", cors(corsOptions));
 
 app.use(express.json());
 app.use(cookieParser());
